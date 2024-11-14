9337a3ec53923a4d7831d8a3364878428e15a0dd
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-11-15 00:00:38 +0300

    class-transformer package add

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as morgan from 'morgan';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
