8e4520eb7a3f17163aaea9eef4bf87c6875cac61
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-11-14 23:58:00 +0300

    class-validator use for security

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,9 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, Matches, MinLength } from 'class-validator';
 
 export class AuthDTO {
   @ApiProperty({ example: 'test@example.com' })
+  @IsEmail({}, { message: 'Enter a valid e-mail address.' })
   email: string;
 
   @ApiProperty({ example: '123456' })
+  @MinLength(8, { message: 'password must have a minimum of 8 characters' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'The password must contain at least one upper case letter, one lower case letter, one number and one special character.',
+    },
+  )
   password: string;
 }
