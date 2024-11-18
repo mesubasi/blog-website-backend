c632b1f8e663a660c8d7096c3f7798c6bbabda42
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-11-18 22:46:05 +0300

    dto initialized

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
+import { BlogDto } from './dto/blog.dto';
 
 @Controller('blogs')
 export class BlogsController {
   constructor(private blogService: BlogsService) {}
 
   @Post()
-  createBlog(@Body() dto) {
+  createBlog(@Body() dto: BlogDto) {
     return this.blogService.createBlog(dto);
   }
 }
