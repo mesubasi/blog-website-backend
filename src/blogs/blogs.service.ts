827f613ac4f4882fb2704775818c7839db1fb6fc
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-11-18 22:53:30 +0300

    blog content save procs add

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Blog, BlogDocument } from './schemas/blog.schema';
 import { Model } from 'mongoose';
-import { JwtService } from '@nestjs/jwt';
+import { BlogDto } from './dto/blog.dto';
 
 @Injectable()
 export class BlogsService {
-  createBlog(dto: any) {
-      throw new Error('Method not implemented.');
-  }
   constructor(@InjectModel(Blog.name) private blogModel: Model<BlogDocument>) {}
+
+  async createBlog(dto: BlogDto) {
+    const newBlog = new this.blogModel({
+      title: dto.title,
+      content: dto.content,
+    });
+    return await newBlog.save();
+  }
 }
