431453cee3d0e92bebde452ccc1a01840bdda0c5
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-11-19 23:35:17 +0300

    guard add

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogDto } from './dto/blog.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('blogs')
 export class BlogsController {
   constructor(private blogService: BlogsService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
-  createBlog(@Body() dto: BlogDto) {
-    return this.blogService.createBlog(dto);
+  createBlog(@Body() dto: BlogDto, @Request() req: any) {
+    return this.blogService.createBlog(dto, req);
   }
 }
--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -3,12 +3,17 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Blog, BlogDocument } from './schemas/blog.schema';
 import { Model } from 'mongoose';
 import { BlogDto } from './dto/blog.dto';
+import { UserDocument } from 'src/auth/schemas/user.schema';
 
 @Injectable()
 export class BlogsService {
-  constructor(@InjectModel(Blog.name) private blogModel: Model<BlogDocument>) {}
+  constructor(
+    @InjectModel(Blog.name) private blogModel: Model<BlogDocument>,
+    @InjectModel(Blog.name) private userModel: Model<UserDocument>,
+  ) {}
 
-  async createBlog(dto: BlogDto) {
+  async createBlog(dto: BlogDto, req: any) {
+    const user = this.userModel.findOne({ email: req.user.email });
     const newBlog = new this.blogModel({
       title: dto.title,
       content: dto.content,
