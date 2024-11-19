adf14c3197439e5d4628fa5610f1f1795c0014a5
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-11-19 23:44:06 +0300

    blog endpoint shared user and authguard edits

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -3,20 +3,22 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Blog, BlogDocument } from './schemas/blog.schema';
 import { Model } from 'mongoose';
 import { BlogDto } from './dto/blog.dto';
-import { UserDocument } from 'src/auth/schemas/user.schema';
+import { User, UserDocument } from 'src/auth/schemas/user.schema';
 
 @Injectable()
 export class BlogsService {
   constructor(
     @InjectModel(Blog.name) private blogModel: Model<BlogDocument>,
-    @InjectModel(Blog.name) private userModel: Model<UserDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
   ) {}
 
   async createBlog(dto: BlogDto, req: any) {
-    const user = this.userModel.findOne({ email: req.user.email });
+    const user = await this.userModel.findOne({ email: req.user.email });
     const newBlog = new this.blogModel({
       title: dto.title,
       content: dto.content,
+      sharedBy: user.email,
+      userId: user._id,
     });
     return await newBlog.save();
   }
