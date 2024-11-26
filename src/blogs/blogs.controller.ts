090cd6546b27d5445c4004ea6baa6369ee0df723
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-11-26 23:55:20 +0300

    Swagger docs edits

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -12,24 +12,29 @@ import {
 import { BlogsService } from './blogs.service';
 import { BlogDto } from './dto/blog.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 
 @Controller('blogs')
 export class BlogsController {
   constructor(private blogService: BlogsService) {}
 
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'Create Successfully!' })
   @Post()
   createBlog(@Body() dto: BlogDto, @Request() req: any) {
     return this.blogService.createBlog(dto, req);
   }
 
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @Put(':id')
   updateBlog(@Body() dto: BlogDto, @Param('id') id: string) {
     return this.blogService.updateBlog(dto, id);
   }
 
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @Delete(':id')
   removeBlog(@Param('id') id: string) {
     return this.blogService.removeBlog(id);
@@ -41,6 +46,7 @@ export class BlogsController {
   }
 
   @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @Get('my-blogs')
   getCurrentUsersBlog(@Request() req: any) {
     return this.blogService.getCurrentUsersBlog(req);
--- src/blogs/dto/blog.dto.ts
+++ src/blogs/dto/blog.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class BlogDto {
+  @ApiProperty({ example: 'Title' })
   title: string;
+  @ApiProperty({ example: 'Description' })
   content: string;
 }
